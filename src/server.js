5fd1ed433cb9432f5a7c913eb544d42fb0a55fb3
Michael Zwierzanski <69387856+mz106@users.noreply.github.com>
2024-07-01 13:47:50 +0100

    remove static routes

--- src/server.js
+++ src/server.js
@@ -2,10 +2,6 @@ const express = require("express");
 
 const app = express();
 
-app.use("/example", express.static("example"));
-
-app.use("/dave", () => console.log("hello dave"));
-
 app.listen(5001, () => {
   console.log(`Server is listening of port 5001`);
 });
