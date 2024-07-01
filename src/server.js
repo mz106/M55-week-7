19a7115691d6af7dfec10954d82f911699cd1993
Michael Zwierzanski <69387856+mz106@users.noreply.github.com>
2024-07-01 17:17:55 +0100

    added solutions

--- src/server.js
+++ src/server.js
@@ -44,14 +44,56 @@ app.post("/books/addBook", (request, response) => {
   response.send(successResponse);
 });
 
-app.put("/books", (request, reponse) => {
+app.put("/books", (request, response) => {
   // in here, find a book by title (i.e. an element of fakeArr where the element title is the same as request.body.title)
   // change (update) the author to an new name
+  console.log(request.body);
+  function findBook(x) {
+    return x.title === request.body.title;
+  }
+
+  const index = fakeData.findIndex(findBook);
+
+  if (index === -1) {
+    const failureResponse = {
+      message: `${request.body.title} not found`,
+    };
+    response.send(failureResponse);
+    return;
+  }
+
+  fakeData[index].title = request.body.newTitle;
+
+  const successResponse = {
+    message: "success",
+    books: fakeData[index],
+  };
+
+  response.send(successResponse);
 });
 
 app.delete("/books", (request, response) => {
   // in here, find a book by title (i.e. an element of fakeArr where the element title is the same as request.body.title)
   // remove (delete) the element from the array
+
+  const index = fakeData.findIndex((book) => book.title === request.body.title);
+
+  if (index === -1) {
+    const failureResponse = {
+      message: `${resuest.body.title} not found`,
+    };
+    response.send(failureResponse);
+    return;
+  }
+
+  fakeData.splice(index, 1);
+
+  const successResponse = {
+    message: "success",
+    books: fakeData,
+  };
+
+  response.send(successResponse);
 });
 
 app.listen(5001, () => {
