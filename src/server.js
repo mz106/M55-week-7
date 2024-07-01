c80769bb738241b4bca7bf142b603dd178a5ac8c
Michael Zwierzanski <69387856+mz106@users.noreply.github.com>
2024-07-01 13:40:13 +0100

    express static finished

--- src/server.js
+++ src/server.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.use("/example", express.static("example"));
+
+app.use("/dave", () => console.log("hello dave"));
+
 app.listen(5001, () => {
   console.log(`Server is listening of port 5001`);
 });
