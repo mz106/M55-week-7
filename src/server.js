4f34c2fb00c167a05dcc0446f276ef80b6a8b06f
Michael Zwierzanski <69387856+mz106@users.noreply.github.com>
2024-07-02 13:59:07 +0100

    add addBook route

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 
@@ -5,11 +6,52 @@ const app = express();
 
 app.use(express.json());
 
+const connection = async () => {
+  await mongoose.connect(process.env.MONGO_URI);
+  console.log("DB is working");
+};
+
+connection();
+
+// Book Model================
+
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  genre: {
+    type: String,
+  },
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+// Book model ends===========
+
 // getAllBooks
 app.get("/books/getAllBooks", (request, response) => {});
 
 // addBook
-app.post("/books/addBook", (request, response) => {});
+app.post("/books/addBook", async (request, response) => {
+  const book = await Book.create({
+    title: request.body.title,
+    author: request.body.author,
+    genre: request.body.genre,
+  });
+
+  const successResponse = {
+    message: "success",
+    book: book,
+  };
+
+  response.send(successResponse);
+});
 
 //update a book's author by title
 app.put("/books", (request, response) => {});
